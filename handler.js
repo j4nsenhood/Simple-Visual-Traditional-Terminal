0fa63fbefe8db7cb5b5d87176aa8fcce86ddce88
jnsn_hood <jansen.dewliewhood@gmail.com>
2026-03-26 16:23:09 +0800

    push it

---
+++ handler.js
@@ -0,0 +1,29 @@
+function Circle(nameId, cont) {
+  var cir = document.getElementById(nameId);
+  var ctx = cir.getContext(cont);
+  ctx.beginPath();
+  ctx.arc(50, 50, 30, 0, 2 * Math.PI);
+  ctx.stroke();
+}
+function tes() {
+  console.log("ayama");
+}
+
+async function terminalIdle(name) {
+  let choice = 1;
+  switch (choice) {
+    case 1:
+      document.getElementById(name).innerHTML = "<";
+      await delay(1000);
+      choice = 2;
+      break;
+    case 2:
+      document.getElementById(name).innerHTML = "<_";
+      await delay(1000);
+      choice = 1;
+      break;
+  }
+}
+
+terminalIdle(terminaltext);
+Circle("draw", "2d");
